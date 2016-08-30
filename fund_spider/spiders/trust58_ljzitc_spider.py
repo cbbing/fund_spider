cdc3bab600ee314cf8d59058491b654bf900014d
cbbing <cbbing@163.com>
2016-08-30 16:54:26 +0800

    59

--- fund_spider/spiders/trust58_ljzitc_spider.py
+++ fund_spider/spiders/trust58_ljzitc_spider.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-爱建信托
+陆家嘴信托
 """
 import sys
 reload(sys)
@@ -11,30 +11,31 @@ from bs4 import BeautifulSoup
 from bs4 import BeautifulSoup as bs
 import re,json
 import hashlib
+# from webHelper import get_requests
 from scrapy.http import FormRequest
 from fund_spider.items import FundSpiderItem
 from util.codeConvert import GetNowTime
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider,Rule
 class TrustSxxtSpider(scrapy.Spider):
-    name = "trust59_ajxt_spider"
-    allowed_domains = ["ajxt.com.cn"]
+    name = "trust58_ljzitc_spider"
+    allowed_domains = ["ljzitc.com.cn"]
 
     start_urls = (
-        'http://www.ajxt.com.cn/Channel/3755',
+        'http://www.ljzitc.com.cn/news/cpjz/index.html',
     )
 
 
     def parse(self, response):
         self.log(response.url)
-        # html = requests.get(response.url)
+        # html = get_requests(response.url)
         soup = BeautifulSoup(response.body, "lxml")
-        trs = soup.find('div', {"class": "page"}).find_all("a")
-        t = trs[8]['href']
-        ts = filter(str.isdigit, t)
-        for i in range(1,int(ts)+1):
-            url = "http://www.ajxt.com.cn/Channel/3755?_tp_ptpro=%d"%i
-            yield scrapy.Request(url, callback=self.parse_history_nav)
+        trs = soup.find_all("tr")
+        for tr in trs:
+            tds=tr.find_all("td")
+            if len(tds)==3:
+                url = "http://www.ljzitc.com.cn"+tds[0].a["href"]
+                yield scrapy.Request(url, callback=self.parse_history_nav)
 
 
 
@@ -46,31 +47,21 @@ class TrustSxxtSpider(scrapy.Spider):
         :return:
         """
         self.log(response.url)
-        # html = requests.get(response.url)
+        # html = get_requests(response.url)
         soup = BeautifulSoup(response.body, "lxml")
         trs = soup.find_all('tr')
         for tr in trs:
-            tds = tr.find_all('td')
-            if len(tds)==3:
+            tds = tr.find_all('td',{"class":"tableProcContent"})
+            if len(tds)==2:
                 item = FundSpiderItem()
-                # if "2015" in tds[1].text.strip():
-                #     item['statistic_date'] = tds[1].text.strip()
-                #     item['nav'] = tds[2].text.strip()
-                # elif "2016" in tds[1].text.strip():
-                #     item['statistic_date'] = tds[1].text.strip()
-                #     item['nav'] = tds[2].text.strip()
-                # else:
+                item['fund_full_name'] = soup.title.text.encode("ISO-8859-1")
+                item['fund_name'] = soup.title.text.encode("ISO-8859-1")
                 item['nav'] = tds[1].text.strip()
-                if tds[2].text.strip() is None:
-                    item['statistic_date'] = None
-                else:
-                    item['statistic_date'] =tds[2].text.strip()
-                item['fund_name'] = tds[0].text.strip()
-                item['fund_full_name'] = item['fund_name']
+                item['statistic_date'] = tds[0].text.strip()
                 item['entry_time'] = GetNowTime()
                 item['source_code'] = 1
                 item['source'] = response.url
-                item['org_id'] = "TG0059"
+                item['org_id'] = "TG0058"
                 item['uuid'] = hashlib.md5((item['fund_name'] + item['statistic_date']).encode('utf8')).hexdigest()
                 print item
                 yield item
