ec8a6365b779df91a08a982992fbb160d0721d3d
cbbing <cbbing@163.com>
2016-09-08 15:02:31 +0800

    58

--- fund_spider/spiders/trust58_ljzitc_spider.py
+++ fund_spider/spiders/trust58_ljzitc_spider.py
@@ -14,9 +14,25 @@ class TrustSxxtSpider(scrapy.Spider):
     name = "trust58_spider"
     allowed_domains = ["ljzitc.com.cn"]
 
-    start_urls = (
-        'http://www.ljzitc.com.cn/news/cpjz/index.html',
-    )
+    # start_urls = (
+    #     'http://www.ljzitc.com.cn/news/cpjz/index.html',
+    # )
+
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, sdch",
+        "Accept-Language": "zh-CN,zh;q=0.8",
+        "Connection": "keep-alive",
+        "Cookie": "Hm_lvt_28e102dd14491ddb45aedf4e66d575e4=1472521033,1473304336; Hm_lpvt_28e102dd14491ddb45aedf4e66d575e4=1473313968",
+        "Host": "www.ljzitc.com.cn",
+        "If-Modified-Since": "Tue, 06 Sep 2016 08:35:34 GMT",
+        "If-None-Match": "83fcc9a2198d21:0",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
+    }
+
+    def start_requests(self):
+        return [scrapy.Request(url='http://www.ljzitc.com.cn/news/cpjz/index.html', headers=self.headers)]
 
 
     def parse(self, response):
@@ -27,7 +43,7 @@ class TrustSxxtSpider(scrapy.Spider):
             tds=tr.find_all("td")
             if len(tds)==3:
                 url = "http://www.ljzitc.com.cn"+tds[0].a["href"]
-                yield scrapy.Request(url, callback=self.parse_history_nav)
+                yield scrapy.Request(url, headers=self.headers, callback=self.parse_history_nav)
 
 
 
